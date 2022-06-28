bafe278059f0b4f71efdda5ecec90900166aa808
Jackson Ming Hu <huming2207@gmail.com>
2022-06-28 23:04:15 +1000

    characteristic_uuids: init

---
+++ lib/common/characteristic_uuids.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+
+class CharacteristicUuids {
+  static final service = Uuid.parse('6ae00001-efc9-11ec-8ea0-0242ac120002');
+  static final configRead = Uuid.parse('6ae00002-efc9-11ec-8ea0-0242ac120002');
+  static final configWrite = Uuid.parse('6ae00003-efc9-11ec-8ea0-0242ac120002');
+  static final uartRx = Uuid.parse('6ae00004-efc9-11ec-8ea0-0242ac120002');
+  static final uartTx = Uuid.parse('6ae00005-efc9-11ec-8ea0-0242ac120002');
+  static final timeSync = Uuid.parse('6ae00006-efc9-11ec-8ea0-0242ac120002');
+}
