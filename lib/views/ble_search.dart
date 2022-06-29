47692e9175f5066461c2f21854b2711cd81e40cb
Jackson Ming Hu <huming2207@gmail.com>
2022-06-29 22:45:30 +1000

    ble_search_page: adapt latest controller logic

--- lib/views/ble_search.dart
+++ lib/views/ble_search.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:get/get.dart';
 import 'package:soul_inspector_app/common/characteristic_uuids.dart';
-import 'package:soul_inspector_app/controller/ble_search_controller.dart';
+import 'package:soul_inspector_app/controller/ble_controller.dart';
 import 'package:soul_inspector_app/controller/main_controller.dart';
 
-class BleSearchPage extends GetView<BleSearchController> {
+class BleSearchPage extends GetView<BleController> {
   final flutterReactiveBle = FlutterReactiveBle();
   final errorSnackBar = const SnackBar(content: Text('Failed to scan device!'), backgroundColor: Colors.red);
   final mainController = Get.find<MainController>();
 
   BleSearchPage({Key? key}) : super(key: key) {
+    controller.clearDevice();
     flutterReactiveBle.scanForDevices(withServices: [CharacteristicUuids.service], scanMode: ScanMode.balanced).listen((device) {
       print('Got device: $device');
       controller.addDevice(device);
@@ -37,12 +38,11 @@ class BleSearchPage extends GetView<BleSearchController> {
                             subtitle: Text('ID: ${device.id}; RSSI: ${device.rssi}'),
                             onTap: () async {
                               controller.setSelectedDevice(device);
-                              mainController.selectedDeviceId.value = device.id;
 
-                              await flutterReactiveBle.requestConnectionPriority(deviceId: device.id, priority: ConnectionPriority.highPerformance);
                               final timeSyncCharacteristic = QualifiedCharacteristic(characteristicId: CharacteristicUuids.timeSync, serviceId: CharacteristicUuids.service, deviceId: device.id);
 
                               final ts = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+                              print('Time now: $ts');
                               final value = [(ts & 0xff), (ts >> 8) & 0xff, (ts >> 16) & 0xff, (ts >> 24) & 0xff];
                               await flutterReactiveBle.writeCharacteristicWithResponse(timeSyncCharacteristic, value: value);
                             }));
