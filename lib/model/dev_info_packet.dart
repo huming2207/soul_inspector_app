c0270d89cd2eafab6117f7941ec9875d16618550
Jackson Ming Hu <huming2207@gmail.com>
2022-07-09 17:27:41 +1000

    dev_info_pkt: init

---
+++ lib/model/dev_info_packet.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+class DevInfoPacket {
+  Uint8List macAddr = Uint8List(6);
+  Uint8List flashId = Uint8List(8);
+  String sdkVer = '';
+  String devModel = '';
+  String devFirmwareVer = '';
+  
+  DevInfoPacket(Uint8List bytes) {
+    macAddr = bytes.sublist(0, 6);
+    flashId = bytes.sublist(6, 14);
+    sdkVer = ascii.decode(bytes.sublist(14, 46));
+    devModel = ascii.decode(bytes.sublist(46, 78));
+    devFirmwareVer = ascii.decode(bytes.sublist(78, 110));
+  }
+}
