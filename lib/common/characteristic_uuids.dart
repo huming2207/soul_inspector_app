cabab699b7e138d899e32d8c5b0861c3df49e0db
Jackson Ming Hu <huming2207@gmail.com>
2022-07-13 22:48:41 +1000

    Add dev info

--- lib/common/characteristic_uuids.dart
+++ lib/common/characteristic_uuids.dart
@@ -7,4 +7,5 @@ class CharacteristicUuids {
   static final uartRx = Uuid.parse('6ae00004-efc9-11ec-8ea0-0242ac120002');
   static final uartTx = Uuid.parse('6ae00005-efc9-11ec-8ea0-0242ac120002');
   static final timeSync = Uuid.parse('6ae00006-efc9-11ec-8ea0-0242ac120002');
+  static final devInfo = Uuid.parse('6ae00007-efc9-11ec-8ea0-0242ac120002');
 }
